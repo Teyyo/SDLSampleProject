c2433742d7eee1ec56320ab6b9e5619b3737d371
Teyyo <64683483+Teyyo@users.noreply.github.com>
2023-03-17 14:22:51 +0100

    Add files via upload

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <iostream>
+
+int main(int argc, char* args[]) {
+
+	std::cout << "SDL Sample Project by Laci\n\nWorking!" << std::endl;
+	std::cin.get();
+
+	return 0;
+}
